c81182ee2c7796b7f990eee7051133c6cd67f618
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2018-07-23 09:27:56 -0400

    Added project

---
+++ java-workspace/java-project/src/me/test/MySample.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package me.test;
+
+/**
+ * @author VarathM
+ *
+ */
+public class MySample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test----Test");
+
+	}
+
+	/**
+	 *
+	 */
+	public MySample() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ java-workspace/java-project/src/me/test/Test.java
@@ -0,0 +1,19 @@
+/**
+ *VarathM on Jul 23, 2018
+ */
+package me.test;
+
+/**
+ * @author VarathM
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
